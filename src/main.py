905d25a436713aa5faca82df3c4832e6a38c89a5
hbiegacz <biegacz.hanna@gmail.com>
2026-04-12 14:57:21 +0200

    implemented inspections in main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from scanner import build_file_index, scan_directories
+from settings import load_settings, parse_permissions
 from arg_parser import parse_arguments
-from settings import load_settings
 from pathlib import Path
+from inspector import *
 
 def main():
     # --- load config ---
@@ -11,7 +12,6 @@ def main():
     args = parse_arguments()
     base = Path(args.base)
     sources = [Path(s) for s in args.sources]
-    dry_run = args.dry_run
 
     if args.dry_run:
         print("\n[dry-run] No changes will be applied.")
@@ -22,52 +22,52 @@ def main():
         args.warnings
     )
     print(f"\nScanning base: {base} and sources: {sources}")
-    # --- inspect & execute ---
 
     if args.all or args.remove_empty:
-        # empty = find_empty_files(files)
+        empty = find_empty_files(files)
         # delete_files(empty, args.dry_run)
         print("\n[inspector] Looking for empty files...")
         print("[executor]  Deleting empty files...")
 
     if args.all or args.remove_temporary:
-        # temp = find_temporary_files(files)
+        temp = find_temporary_files(files, settings["TEMP_FILE_PATTERNS"])
         # delete_files(temp, args.dry_run)
         print("\n[inspector] Looking for temporary files...")
         print("[executor]  Deleting temporary files...")
 
     if args.all or args.remove_duplicates:
-        # duplicates = find_duplicates(files)
+        duplicates = find_duplicates(files)
         # resolve_duplicates(duplicates, args.dry_run)
         print("\n[inspector] Looking for duplicate files...")
         print("[executor]  Resolving duplicates...")
 
     if args.all or args.resolve_conflicts:
-        # conflicts = find_name_conflicts(files)
+        conflicts = find_name_conflicts(files)
         # resolve_name_conflict(conflicts, args.dry_run)
         print("\n[inspector] Looking for name conflicts...")
         print("[executor]  Resolving name conflicts...")
 
     if args.all or args.fix_permissions:
-        # bad_perms = find_bad_permissions(files)
+        target_permissions = parse_permissions(settings["TARGET_PERMISSIONS"])
+        bad_perms = find_bad_permissions(files, target_permissions)
         # fix_permissions(bad_perms, args.dry_run)
         print("\n[inspector] Looking for bad permissions...")
         print("[executor]  Fixing permissions...")
 
     if args.all or args.fix_suspicious_names:
-        # suspicious = find_suspicious_names(files)
+        suspicious = find_suspicious_names(files, settings["SUSPICIOUS_CHARS"])
         # rename_file(suspicious, args.dry_run)
         print("\n[inspector] Looking for suspicious file names...")
         print("[executor]  Renaming files...")
 
     if args.all or args.copy_missing:
-        # missing = find_missing_files(base, sources, files)
+        missing = find_missing_files(base, sources, files)
         # copy_missing_to_base(missing, base, args.dry_run)
         print("\n[inspector] Looking for files missing from base...")
         print("[executor]  Copying missing files to base...")
 
     if args.move_missing:
-        # missing = find_missing_files(base, sources, files)
+        missing = find_missing_files(base, sources, files)
         # move_file_to_base(missing, base, args.dry_run)
         print("\n[inspector] Looking for files missing from base...")
         print("[executor]  Moving missing files to base...")
